a1be735228ec953e473a054a2c89658b195a2d62
3loomin <3loomin@gmail.com>
2022-01-06 20:53:31 +0900

    2단계 2553번 문제

--- Level2/main.c
+++ Level2/main.c
@@ -1,6 +1,6 @@
 
 
-#if 1
+#if 0
 //1330
 #include <stdio.h>
 int main() {
@@ -10,6 +10,38 @@ int main() {
     if(a>b) printf(">\n");
     if(a<b) printf("<\n");
     if(a==b) printf("==\n");
+    return 0;
+}
+#endif
+
+#if 0
+//9498
+#include <stdio.h>
+int main() {
+    int score = 0;
+
+    scanf("%d", &score);
+    if(score>=90) printf("A\n");
+    else if(score>=80) printf("B\n");
+    else if(score>=70) printf("C\n");
+    else if(score>=60) printf("D\n");
+    else printf("F\n");
+    return 0;
+}
+#endif
+
+#if 1
+//2753
+#include <stdio.h>
+int main() {
+    int year = 0;
+    scanf("%d", &year);
+    if(year%4 == 0){
+        if(year%100 != 0 || year%400 == 0)
+            printf("1\n");
+        else printf("0\n");
+    }
+    else printf("0\n");
     return 0;
 }
 #endif
