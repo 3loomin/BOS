da2a314a2d4c8f68d1d72ff321a48c3fd85187f4
3loomin <3loomin@gmail.com>
2022-01-06 20:41:17 +0900

    2단계 1330번 문제

---
+++ Level2/cmake-build-debug/CMakeFiles/Level2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Level2.dir/main.c.o"
+  "Level2"
+  "Level2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Level2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Level2/main.c
@@ -0,0 +1,15 @@
+
+
+#if 1
+//1330
+#include <stdio.h>
+int main() {
+    int a = 0;
+    int b = 0;
+    scanf("%d %d", &a, &b);
+    if(a>b) printf(">\n");
+    if(a<b) printf("<\n");
+    if(a==b) printf("==\n");
+    return 0;
+}
+#endif
