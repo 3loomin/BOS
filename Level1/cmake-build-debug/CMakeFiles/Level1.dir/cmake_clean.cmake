057451539e70b06f8c00ee2e10cdebdfc9221cf3
3loomin <3loomin@gmail.com>
2022-01-06 18:58:21 +0900

    1단계 3번 문제

---
+++ Level1/cmake-build-debug/CMakeFiles/Level1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Level1.dir/main.c.o"
+  "Level1"
+  "Level1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Level1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Level1/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#if 0
+//2557
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
+#endif
+
+#if 0
+//10718
+int main() {
+    printf("강한친구 대한육군\n");
+    printf("강한친구 대한육군\n");
+    return 0;
+}
+#endif
+#if 1
+//10171
+int main(){
+    printf("\\    /\\\n"
+           " )  ( ')\n"
+           "(  /  )\n"
+           " \\(__)|");
+}
+#endif
+
