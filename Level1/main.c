f781910bd0c83c86c020d5fc2abb880a066145b7
3loomin <3loomin@gmail.com>
2022-01-06 19:06:05 +0900

    1단계 1001번 문제

--- Level1/main.c
+++ Level1/main.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
 #if 0
 //2557
+#include <stdio.h>
 int main() {
     printf("Hello World!\n");
     return 0;
@@ -9,6 +9,7 @@ int main() {
 
 #if 0
 //10718
+#include <stdio.h>
 int main() {
     printf("강한친구 대한육군\n");
     printf("강한친구 대한육군\n");
@@ -17,6 +18,7 @@ int main() {
 #endif
 #if 0
 //10171
+#include <stdio.h>
 int main(){
     printf("\\    /\\\n"
            " )  ( ')\n"
@@ -25,13 +27,38 @@ int main(){
 }
 #endif
 
-#if 1
+#if 0
 //10172
+#include <stdio.h>
 int main(){
     printf("|\\_/|\n"
            "|q p|   /}\n"
            "( 0 )\"\"\"\\\n"
            "|\"^\"`    |\n"
            "||_/=\\\\__|");
+}
+#endif
+
+#if 0
+//1000
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(){
+    int a = 0;
+    int b = 0;
+    scanf("%d %d",&a,&b);
+    printf("%d",a+b);
+}
+#endif
+
+#if 1
+//1001
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(){
+    int a = 0;
+    int b = 0;
+    scanf("%d %d",&a,&b);
+    printf("%d",a-b);
 }
 #endif
