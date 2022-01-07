a7586c986770c463993afe6c2597d0cc5357cf66
hyeseong <hyeseong.ee@gmail.com>
2022-01-07 17:09:12 +0900

    4단계 10951문제

--- Level4/main.c
+++ Level4/main.c
@@ -1,4 +1,4 @@
-#if 1
+#if 0
 //10952
 #define CRT_NO_SECURE_WARNINGS
 #include <stdio.h>
@@ -7,6 +7,22 @@ int main() {
     int b = 0;
 
     while(1){
+        scanf("%d %d", &a, &b);
+        if(a == 0 && b ==0) break;
+        else printf("%d\n",a + b);
+    }
+    return 0;
+}
+#endif
+
+#if 1
+//10951
+#define CRT_NO_SECURE_WARNINGS
+#include <stdio.h>
+int main() {
+    while(1){
+        int a = 0;
+        int b = 0;
         scanf("%d %d", &a, &b);
         if(a == 0 && b ==0) break;
         else printf("%d\n",a + b);
