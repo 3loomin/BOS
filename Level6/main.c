d6c08707c3f4642320585dcf6aca26046cb34e10
3loomin <3loomin@gmail.com>
2022-01-14 23:15:30 +0900

    6단계 4673번 문제

--- Level6/main.c
+++ Level6/main.c
@@ -1,4 +1,4 @@
-#if 1
+#if 0
 //15596
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
@@ -25,3 +25,32 @@ int main() {
     return 0;
 }
 #endif
+#if 1
+//4673
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+void d(int a,int n, int* arr){
+    int temp = a;
+    while(n > 0){
+        temp += a % 10;
+        a = a / 10;
+        n--;
+    }
+    arr[temp] = 1;
+    int b = log10(temp) + 1;
+    if(temp < 100) d(temp,b,arr);
+}
+int main() {
+    int arr[10100] = {0,};
+    for(int i = 1; i < 10000; i++){
+        int n = log10(i) + 1;
+        d(i, n, arr);
+    }
+    for(int j = 1; j < 10000; j++){
+        if(arr[j] == 0) printf("%d\n",j);
+    }
+    return 0;
+}
+#endif
+
