231a339df8347becc41b314410c2115b82497870
3loomin <3loomin@gmail.com>
2022-01-17 23:03:32 +0900

    7단계 10809, 2675번 문제

--- Level7/main.c
+++ Level7/main.c
@@ -10,7 +10,7 @@ int main() {
 }
 #endif
 
-#if 1
+#if 0
 //11720
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
@@ -24,6 +24,62 @@ int main() {
         sum += arr[i] - '0';
     }
     printf("%d", sum);
+    return 0;
+}
+#endif
+
+#if 0
+//10809
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+    char ans[100] = {0,};
+    int alpha[26];
+    for(int k = 0; k < 26; k++){
+        alpha[k] = -1;
+    }
+    int k = 99;
+    scanf("%s",&ans);
+    while(k >= 0){
+        alpha[ans[k] - 'a'] = k;
+        k--;
+    }
+    for(int j = 0; j < 26; j++){
+        printf("%d ",alpha[j]);
+    }
+    return 0;
+}
+#endif
+
+#if 1
+//2675
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+    int testcase = 0;
+    scanf("%d",&testcase);
+    for(int i = 0; i < testcase; i++){
+        int n = 0;
+        scanf("%d",&n);
+
+        char arr[1000] = {0,};
+        scanf("%s",&arr);
+
+        char ans[1000] = {0,};
+        int j = 0;
+        int idx = 0;
+
+        while(arr[j] != 0){
+            int k = 0;
+            while(k < n ){
+                ans[idx] = arr[j];
+                idx++;
+                k++;
+            }
+            j++;
+        }
+        printf("%s\n",ans);
+    }
     return 0;
 }
 #endif
