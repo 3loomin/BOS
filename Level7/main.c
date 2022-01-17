6a69e0d9920bd10f5d58a064a3033a8d8c58ddaf
3loomin <3loomin@gmail.com>
2022-01-17 21:31:35 +0900

    7단계 11720번 문제

--- Level7/main.c
+++ Level7/main.c
@@ -1,4 +1,4 @@
-#if 1
+#if 0
 //11654
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
@@ -6,6 +6,24 @@ int main() {
     char a = ' ';
     scanf("%c",&a);
     printf("%d",a);
+    return 0;
+}
+#endif
+
+#if 1
+//11720
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+    int n = 0;
+    char arr[100] ={0};
+    int sum = 0;
+    scanf("%d",&n);
+    scanf("%s", &arr);
+    for (int i = 0; i < n; i++){
+        sum += arr[i] - '0';
+    }
+    printf("%d", sum);
     return 0;
 }
 #endif
